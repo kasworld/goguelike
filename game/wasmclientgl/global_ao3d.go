18ce650228c67d3e75050a2c1398511352a6bd4c
SeukWon Kang <kasworld@gmail.com>
2020-07-09 16:17:39 +0900

    prepare activeobjgl glclient

--- game/wasmclientgl/global_ao3d.go
+++ game/wasmclientgl/global_ao3d.go
@@ -10,3 +10,64 @@
 // limitations under the License.
 
 package wasmclientgl
+
+import (
+	"syscall/js"
+
+	"github.com/kasworld/goguelike/lib/webtilegroup"
+	"github.com/kasworld/goguelike/protocol_c2t/c2t_obj"
+)
+
+type ActiveObjGL struct {
+	Cnv     js.Value
+	Ctx     js.Value
+	Tex     js.Value
+	GeoInfo GeoInfo
+	Mesh    js.Value
+}
+
+func NewActiveObjGL(ao *c2t_obj.ActiveObjClient) *ActiveObjGL {
+	tlList := gClientTile.CharTiles[ao.Faction]
+	ti := tlList[0]
+
+	cnv := js.Global().Get("document").Call("createElement", "CANVAS")
+	ctx := cnv.Call("getContext", "2d")
+	ctx.Set("imageSmoothingEnabled", false)
+	cnv.Set("width", DstCellSize)
+	cnv.Set("height", DstCellSize)
+	ctx.Call("drawImage", gClientTile.TilePNG.Cnv,
+		ti.Rect.X, ti.Rect.Y, ti.Rect.W, ti.Rect.H,
+		0, 0, DstCellSize, DstCellSize)
+
+	tex := ThreeJsNew("CanvasTexture", cnv)
+	mat := ThreeJsNew("MeshPhongMaterial",
+		map[string]interface{}{
+			"map": tex,
+		},
+	)
+	mat.Set("transparent", true)
+	geo := ThreeJsNew("BoxGeometry", DstCellSize, DstCellSize, DstCellSize)
+	mesh := ThreeJsNew("Mesh", geo, mat)
+	return &ActiveObjGL{
+		Cnv:     cnv,
+		Ctx:     ctx,
+		Tex:     tex,
+		GeoInfo: GetGeoInfo(geo),
+		Mesh:    mesh,
+	}
+}
+
+func (aog *ActiveObjGL) ChangeTile(ti webtilegroup.TileInfo) {
+	aog.Ctx.Call("drawImage", gClientTile.TilePNG.Cnv,
+		ti.Rect.X, ti.Rect.Y, ti.Rect.W, ti.Rect.H,
+		0, 0, DstCellSize, DstCellSize)
+	aog.Tex.Set("needsUpdate", true)
+}
+
+func (aog *ActiveObjGL) Dispose() {
+	// mesh do not need dispose
+	aog.Mesh.Get("geometry").Call("dispose")
+	aog.Mesh.Get("material").Call("dispose")
+	aog.Tex.Call("dispose")
+	js.Global().Get("document").Call("removeChild", aog.Cnv)
+}
