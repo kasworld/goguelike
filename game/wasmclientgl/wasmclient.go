b325204faa20b7d34ae5d90e401e64a8c4d3ee7b
SeukWon Kang <kasworld@gmail.com>
2020-07-15 15:46:22 +0900

    update cursor type glclient

--- game/wasmclientgl/wasmclient.go
+++ game/wasmclientgl/wasmclient.go
@@ -309,6 +309,16 @@ func (app *WasmClient) renderGLFrame(this js.Value, args []js.Value) interface{}
 		fx, fy := app.vp.FindRayCastingFxFy()
 		tl := cf.Tiles[cf.XWrapSafe(fx)][cf.YWrapSafe(fy)]
 		height := GetTile3DHeightByCache(tl)
+		if !tl.CharPlaceable() {
+			app.vp.cursor.ChangeTile(gClientTile.CursorTiles[2])
+		} else {
+			if tl.Safe() {
+				app.vp.cursor.ChangeTile(gClientTile.CursorTiles[0])
+
+			} else {
+				app.vp.cursor.ChangeTile(gClientTile.CursorTiles[1])
+			}
+		}
 		app.vp.cursor.SetFieldPosition(fx, fy, height)
 		app.vp.renderer.Call("render", app.vp.scene, app.vp.camera)
 	}
