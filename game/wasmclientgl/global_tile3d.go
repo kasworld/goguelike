e646c3be6f1b6e951104686fe8e474222a7ee7a7
SeukWon Kang <kasworld@gmail.com>
2020-07-15 16:02:45 +0900

    all tile3d shift z +2 for cursor render fix

--- game/wasmclientgl/global_tile3d.go
+++ game/wasmclientgl/global_tile3d.go
@@ -36,178 +36,192 @@ func preMakeTileMatGeo() {
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, -1},
+		Shift: [3]float64{0, 0, 1},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, -1},
+		Shift: [3]float64{0, 0, 1},
 	}
 
 	tlt = tile.Soil
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tlt),
-		Geo: ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Mat:   NewTextureTileMaterial(tlt),
+		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tlt),
-		Geo: ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Mat:   NewTextureTileMaterial(tlt),
+		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Stone
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tlt),
-		Geo: ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Mat:   NewTextureTileMaterial(tlt),
+		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tlt),
-		Geo: ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Mat:   NewTextureTileMaterial(tlt),
+		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Sand
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 0},
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 0},
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Sea
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, -2},
+		Shift: [3]float64{0, 0, 0},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, -2},
+		Shift: [3]float64{0, 0, 0},
 	}
 
 	tlt = tile.Magma
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, -2},
+		Shift: [3]float64{0, 0, 0},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, -2},
+		Shift: [3]float64{0, 0, 0},
 	}
 
 	tlt = tile.Ice
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 1},
+		Shift: [3]float64{0, 0, 3},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 1},
+		Shift: [3]float64{0, 0, 3},
 	}
 
 	tlt = tile.Grass
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tile.Grass),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize/8),
+		Mat:   NewTextureTileMaterial(tile.Grass),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize/8),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tile.Grass),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize/8),
+		Mat:   NewTextureTileMaterial(tile.Grass),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize/8),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Tree
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tile.Grass),
-		Geo: MakeTreeGeo(),
+		Mat:   NewTextureTileMaterial(tile.Grass),
+		Geo:   MakeTreeGeo(),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tile.Grass),
-		Geo: MakeTreeGeo(),
+		Mat:   NewTextureTileMaterial(tile.Grass),
+		Geo:   MakeTreeGeo(),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Road
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 1},
+		Shift: [3]float64{0, 0, 3},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 1},
+		Shift: [3]float64{0, 0, 3},
 	}
 
 	tlt = tile.Room
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 0},
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, 0},
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Wall
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tile.Stone),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Mat:   NewTextureTileMaterial(tile.Stone),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTextureTileMaterial(tile.Stone),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Mat:   NewTextureTileMaterial(tile.Stone),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Window
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTileMaterial(gClientTile.CursorTiles[2]),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Mat:   NewTileMaterial(gClientTile.CursorTiles[2]),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTileMaterial(gClientTile.CursorTiles[2]),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Mat:   NewTileMaterial(gClientTile.CursorTiles[2]),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Door
 	gTile3D[tlt] = Tile3D{
-		Mat: NewTileMaterial(gClientTile.FloorTiles[tile.Door][0]),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize-1),
+		Mat:   NewTileMaterial(gClientTile.FloorTiles[tile.Door][0]),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize-1),
+		Shift: [3]float64{0, 0, 2},
 	}
 	gTile3DDark[tlt] = Tile3D{
-		Mat: NewTileMaterial(gClientTile.FloorTiles[tile.Door][0]),
-		Geo: ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize-1),
+		Mat:   NewTileMaterial(gClientTile.FloorTiles[tile.Door][0]),
+		Geo:   ThreeJsNew("BoxGeometry", DstCellSize-1, DstCellSize-1, DstCellSize-1),
+		Shift: [3]float64{0, 0, 2},
 	}
 
 	tlt = tile.Fog
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, DstCellSize/8 + 1},
+		Shift: [3]float64{0, 0, 4},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, DstCellSize/8 + 1},
+		Shift: [3]float64{0, 0, 4},
 	}
 
 	tlt = tile.Smoke
 	gTile3D[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, DstCellSize/8 + 1},
+		Shift: [3]float64{0, 0, 4},
 	}
 	gTile3DDark[tlt] = Tile3D{
 		Mat:   NewTextureTileMaterial(tlt),
 		Geo:   ThreeJsNew("PlaneGeometry", DstCellSize, DstCellSize),
-		Shift: [3]float64{0, 0, DstCellSize/8 + 1},
+		Shift: [3]float64{0, 0, 4},
 	}
 
 	for i := 0; i < tile.Tile_Count; i++ {
