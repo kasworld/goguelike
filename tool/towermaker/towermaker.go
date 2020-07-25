a29ed77f7a0da06cc023bd9cac979c89cc0b98a8
SeukWon Kang <kasworld@gmail.com>
2020-07-25 11:17:03 +0900

    update towermaker

--- tool/towermaker/towermaker.go
+++ tool/towermaker/towermaker.go
@@ -32,75 +32,26 @@ func main() {
 	makeRogueTower(*towername, *floorcount)
 }
 
+var whList = []int{
+	32, 64, 128,
+}
+var allRoomTile = []string{
+	"Room", "Soil", "Sand", "Stone", "Grass", "Tree", "Ice", "Magma", "Swamp", "Sea", "Smoke",
+}
+var allRoadTile = []string{
+	"Road", "Soil", "Sand", "Stone", "Grass", "Tree", "Fog",
+}
+
+var rnd = g2rand.New()
+
 func makeRogueTower(towerName string, floorCount int) {
-	whList := []int{
-		32, 64, 128,
-	}
-	rnd := g2rand.New()
-	allRoomTile := []string{
-		"Room", "Soil", "Sand", "Stone", "Grass", "Tree", "Ice", "Magma", "Swamp", "Sea", "Smoke",
-	}
-	allRoadTile := []string{
-		"Road", "Soil", "Sand", "Stone", "Grass", "Tree", "Fog",
-	}
 
 	tw := make(towerscript.TowerScript, 0)
 
 	for i := 0; i < floorCount; i++ {
 		w := whList[rnd.Intn(len(whList))]
 		h := whList[rnd.Intn(len(whList))]
-		roomCount := w * h / 512
-		if roomCount < 2 {
-			roomCount = 2
-		}
-		roomTile := allRoomTile[rnd.Intn(len(allRoomTile))]
-		roadTile := allRoadTile[rnd.Intn(len(allRoadTile))]
-		fl := []string{
-			fmt.Sprintf(
-				"NewTerrain w=%v h=%v name=Floor%v ao=%v po=%v actturnboost=1.0",
-				w, h, i, roomCount/2, 0),
-			fmt.Sprintf("AddRoomsRand bgtile=%v walltile=Wall terrace=false align=1 count=%v mean=8 stddev=2 min=6",
-				roomTile, roomCount),
-			fmt.Sprintf("ConnectRooms tile=%v connect=1 allconnect=true diagonal=false",
-				roadTile),
-			"FinalizeTerrain",
-			fmt.Sprintf(
-				"AddPortalsInRoom display=StairDn acttype=PortalInOut PortalID=Floor%v-0 DstPortalID=Floor%v-1 message=Floor%v",
-				i, wrapInt(i-1, floorCount), wrapInt(i-1, floorCount)),
-			fmt.Sprintf(
-				"AddPortalsInRoom display=StairUp acttype=PortalInOut PortalID=Floor%v-1 DstPortalID=Floor%v-0 message=Floor%v",
-				i, wrapInt(i+1, floorCount), wrapInt(i+1, floorCount)),
-			fmt.Sprintf("AddRecyclerInRoom display=Recycler count=%v message=Recycle", roomCount/2),
-			fmt.Sprintf("AddTrapTeleportsInRoom DstFloor=Floor%v count=%v message=Teleport",
-				i, roomCount/2),
-		}
-
-		for trapMade := 0; ; {
-			dstFloor := rnd.Intn(floorCount)
-			cmd := fmt.Sprintf("AddTrapTeleportsInRoom DstFloor=Floor%v count=%v message=ToFloor%v",
-				dstFloor, 1, dstFloor)
-
-			fl = append(fl, cmd)
-			trapMade++
-			if trapMade > roomCount/2 {
-				break
-			}
-		}
-
-		for trapMade := 0; ; {
-			j := rnd.Intn(fieldobjacttype.FieldObjActType_Count)
-			ft := fieldobjacttype.FieldObjActType(j)
-			if fieldobjacttype.GetBuffByFieldObjActType(ft) == nil {
-				continue
-			}
-			cmd := fmt.Sprintf("AddTrapsInRoom display=None acttype=%v count=1 message=%v",
-				ft, ft)
-			fl = append(fl, cmd)
-			trapMade++
-			if trapMade > roomCount {
-				break
-			}
-		}
+		fl := makeRoguelikeFloor(floorCount, i, w, h)
 		tw = append(tw, fl)
 	}
 
@@ -108,6 +59,62 @@ func makeRogueTower(towerName string, floorCount int) {
 	if err != nil {
 		g2log.Error("%v", err)
 	}
+}
+
+func makeRoguelikeFloor(floorCount, i, w, h int) []string {
+	roomCount := w * h / 512
+	if roomCount < 2 {
+		roomCount = 2
+	}
+	roomTile := allRoomTile[rnd.Intn(len(allRoomTile))]
+	roadTile := allRoadTile[rnd.Intn(len(allRoadTile))]
+	fl := []string{
+		fmt.Sprintf(
+			"NewTerrain w=%v h=%v name=Floor%v ao=%v po=%v actturnboost=1.0",
+			w, h, i, roomCount/2, 0),
+		fmt.Sprintf("AddRoomsRand bgtile=%v walltile=Wall terrace=false align=1 count=%v mean=8 stddev=2 min=6",
+			roomTile, roomCount),
+		fmt.Sprintf("ConnectRooms tile=%v connect=1 allconnect=true diagonal=false",
+			roadTile),
+		"FinalizeTerrain",
+		fmt.Sprintf(
+			"AddPortalsInRoom display=StairDn acttype=PortalInOut PortalID=Floor%v-0 DstPortalID=Floor%v-1 message=Floor%v",
+			i, wrapInt(i-1, floorCount), wrapInt(i-1, floorCount)),
+		fmt.Sprintf(
+			"AddPortalsInRoom display=StairUp acttype=PortalInOut PortalID=Floor%v-1 DstPortalID=Floor%v-0 message=Floor%v",
+			i, wrapInt(i+1, floorCount), wrapInt(i+1, floorCount)),
+		fmt.Sprintf("AddRecyclerInRoom display=Recycler count=%v message=Recycle", roomCount/2),
+		fmt.Sprintf("AddTrapTeleportsInRoom DstFloor=Floor%v count=%v message=Teleport",
+			i, roomCount/2),
+	}
+
+	for trapMade := 0; ; {
+		dstFloor := rnd.Intn(floorCount)
+		cmd := fmt.Sprintf("AddTrapTeleportsInRoom DstFloor=Floor%v count=%v message=ToFloor%v",
+			dstFloor, 1, dstFloor)
+
+		fl = append(fl, cmd)
+		trapMade++
+		if trapMade > roomCount/2 {
+			break
+		}
+	}
+
+	for trapMade := 0; ; {
+		j := rnd.Intn(fieldobjacttype.FieldObjActType_Count)
+		ft := fieldobjacttype.FieldObjActType(j)
+		if fieldobjacttype.GetBuffByFieldObjActType(ft) == nil {
+			continue
+		}
+		cmd := fmt.Sprintf("AddTrapsInRoom display=None acttype=%v count=1 message=%v",
+			ft, ft)
+		fl = append(fl, cmd)
+		trapMade++
+		if trapMade > roomCount {
+			break
+		}
+	}
+	return fl
 }
 
 func wrapInt(v, l int) int {
