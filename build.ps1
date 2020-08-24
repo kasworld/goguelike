69c1de6ea8dd4d5ade5c769a2f17a0b2d4c7a90b
Seukwon Kang <kasworld@gmail.com>
2020-08-24 13:23:46 +0900

    update genwasmclient

--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
 
+################################################################################
 $DATESTR=Get-Date -UFormat '+%Y-%m-%dT%H:%M:%S%Z:00'
 $GITSTR=git rev-parse HEAD
 $BUILD_VER=${DATESTR} +"_" +${GITSTR}+"_release_windows"
 Write-Output "Build Version:" ${BUILD_VER}
+
+################################################################################
