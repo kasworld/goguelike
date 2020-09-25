49b3618acc785ddb7cf573e19736bec5aacb7525
Seukwon Kang <kasworld@gmail.com>
2020-09-25 14:45:12 +0900

    update big tower

--- tool/towermaker/bigtower/bigtower.go
+++ tool/towermaker/bigtower/bigtower.go
@@ -15,6 +15,8 @@ import (
 	"fmt"
 
 	"github.com/kasworld/g2rand"
+	"github.com/kasworld/goguelike/config/gameconst"
+	"github.com/kasworld/goguelike/enum/decaytype"
 	"github.com/kasworld/goguelike/tool/towermaker/floortemplate"
 	"github.com/kasworld/goguelike/tool/towermaker/towermake"
 )
@@ -61,18 +63,20 @@ func New(name string, floorCount int) *towermake.Tower {
 			)
 			fm.Appendf("ActiveObjectsRand count=%v", 256)
 		case 7:
-			fm := tw.Add(floorName, 64, 64, 1.0).Appends(
-				floortemplate.FreeForAll64x64()...,
+			fm := tw.Add(floorName, 256, 256, 1.0).Appends(
+				floortemplate.CityRooms(256, 256, 11, 11, 5, rnd.Intn)...,
 			)
-			fm.Appendf("ActiveObjectsRand count=%v", 16)
+			fm.Appendf("ActiveObjectsRand count=%v", 256)
 
 		case 9:
 			fm := tw.Add(floorName, 256, 256, 1.0).Appends(
-				"ResourceFromPNG name=imagefloor.png",
-				"ResourceRand resource=Plant mean=100000000 stddev=65535 repeat=256",
-				"ResourceAgeing initrun=0 msper=64000 resetaftern=1440",
-				"AddRoomsRand bgtile=Room walltile=Wall terrace=false align=1 count=32 mean=6 stddev=4",
-				"ConnectRooms tile=Road connect=1 allconnect=false diagonal=true",
+				fmt.Sprintf("ResourceFillRect resource=Soil amount=1 x=0 y=0 w=%v h=%v", 256, 256),
+			)
+			fm.Appends(
+				floortemplate.MixedResourceMaze(256, 256)...,
+			)
+			fm.Appends(
+				floortemplate.CityRoomsRand(512, rnd.Intn)...,
 			)
 			fm.Appendf("ActiveObjectsRand count=%v", 256)
 		}
@@ -84,25 +88,74 @@ func New(name string, floorCount int) *towermake.Tower {
 		}
 	}
 
+	lhlen := gameconst.ViewPortW / 2
+
 	for i, fm := range tw.GetList() {
 		roomCount := fm.CalcRoomCount()
+		recycleCount := fm.W * fm.H / gameconst.ViewPortWH / 8
+		if recycleCount < 2 {
+			recycleCount = 2
+		}
+
 		suffix1 := "InRoom"
 		if roomCount == 0 {
 			suffix1 = "Rand"
 		}
+
+		fm2 := tw.GetList()[wrapInt(i+1, floorCount)]
 		suffix2 := "InRoom"
-		if tw.GetList()[wrapInt(i+1, floorCount)].CalcRoomCount() == 0 {
+		if fm2.CalcRoomCount() == 0 {
 			suffix2 = "Rand"
 		}
-		fm.ConnectStairUp(suffix1, suffix2, tw.GetList()[wrapInt(i+1, floorCount)])
+		fm.ConnectStairUp(suffix1, suffix2, fm2)
 
-		fm.AddRecycler(suffix1, roomCount/2)
-		fm.AddTeleportIn(suffix1, roomCount/2)
-		for trapMade := 0; trapMade < roomCount/2; trapMade++ {
-			dstFloor := tw.GetList()[rnd.Intn(tw.GetCount())]
-			fm.AddTrapTeleportTo(suffix1, dstFloor)
+		fm2 = tw.GetList()[rnd.Intn(tw.GetCount())]
+		suffix2 = "InRoom"
+		if fm2.CalcRoomCount() == 0 {
+			suffix2 = "Rand"
 		}
-		fm.AddEffectTrap(suffix1, roomCount/2)
+		fm.ConnectPortalTo(suffix1, suffix2, fm2)
+		fm.ConnectAutoInPortalTo(suffix1, suffix1, fm)
+		fm.AddTrapTeleportTo(suffix1, fm)
+		for j := 0; j < recycleCount/8; j++ {
+			fm2 = tw.GetList()[rnd.Intn(tw.GetCount())]
+			suffix2 = "InRoom"
+			if fm2.CalcRoomCount() == 0 {
+				suffix2 = "Rand"
+			}
+			fm.ConnectPortalTo(suffix1, suffix2, fm2)
+
+			fm.ConnectAutoInPortalTo("Rand", "Rand", fm)
+			fm.AddTrapTeleportTo("Rand", fm)
+		}
+		fm.AddAllEffectTrap(suffix1, 1)
+		if count := recycleCount / 32; count > 0 {
+			fm.AddAllEffectTrap("Rand", count)
+		}
+
+		if recycleCount > roomCount {
+			fm.AddRecycler(suffix1, roomCount)
+		} else {
+			fm.AddRecycler(suffix1, recycleCount)
+		}
+		if recycleCount-roomCount > 0 {
+			fm.AddRecycler("Rand", recycleCount-roomCount)
+		}
+		for j := 0; j < decaytype.DecayType_Count; j++ {
+			decay := decaytype.DecayType(j)
+			fm.Appendf(
+				"AddRotateLineAttack%v display=RotateLineAttack winglen=%v wingcount=1 degree=0 perturn=10 decay=%v count=%v message=RotDanger1",
+				"Rand", lhlen, decay, 1,
+			)
+			fm.Appendf(
+				"AddRotateLineAttack%v display=RotateLineAttack winglen=%v wingcount=2 degree=0 perturn=10 decay=%v count=%v message=RotDanger2",
+				"Rand", lhlen/2, decay, 1,
+			)
+			fm.Appendf(
+				"AddMine%v display=None decay=%v count=%v message=Mine",
+				"Rand", decay, 1,
+			)
+		}
 	}
 	return tw
 }
